92695d698c42098fdd25f02dd06720e0e8138f7e
Sungjin01 <43747041+Sungjin01@users.noreply.github.com>
2019-07-16 03:57:32 +0900

    First Commit

---
+++ suap/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public UIManager uimanger;
+    public float moveSpeed;
+    public GameObject Body;
+
+    private float h, v;
+    private Rigidbody2D rigid2d;
+    private Animator anim;
+    private Vector3 moment;
+
+
+    void Start()
+    {
+        rigid2d = GetComponent<Rigidbody2D>();
+        anim = Body.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        Move();
+    }
+
+    private void Move()
+    {
+        anim.SetBool("isRun", !((h == 0)&&(v == 0)));
+
+        moment = new Vector3(h, v, 0).normalized * Time.deltaTime * moveSpeed;
+        rigid2d.MovePosition(moment + transform.position);
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.CompareTag("item"))//tag로 줄일수있다.
+        {
+            uimanger.SetFillAmount(0.3f);
+            coll.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ suap/Assets/Scripts/UIManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Image oil;
+    public Transform lightN;
+
+    private int lightState;
+
+    void Start()
+    {
+        lightState = 0; //초기 상태;
+    }
+
+    void Update()
+    {
+        OilCheck();
+    }
+
+    public void SetFillAmount(float amount)
+    {
+        oil.fillAmount += amount;
+    }
+
+    private void OilCheck()
+    {
+        oil.fillAmount -= 0.0005f;
+
+        if(lightState == 0)
+        {
+            if(oil.fillAmount <= 0.7f)
+            {
+                StartCoroutine(LightFadeInOut(true));
+                lightState = 1;
+                return;
+            }
+
+        }else if(lightState == 1)
+        {
+            if (oil.fillAmount <= 0.3f)
+            {
+                StartCoroutine(LightFadeInOut(true));
+                lightState = 2;
+                return;
+
+            }
+            else if(oil.fillAmount > 0.7f)
+            {
+                StartCoroutine(LightFadeInOut(false));
+                lightState = 0;
+                return;
+            }
+        }
+        else
+        {
+            if (oil.fillAmount > 0.3f)
+            {
+                StartCoroutine(LightFadeInOut(false));
+                lightState = 1;
+                return;
+            }
+        }
+    }
+
+    private IEnumerator LightFadeInOut(bool isFadeIn)
+    {
+        Vector3 lightLocalScale = lightN.localScale;
+        if (isFadeIn)
+        {
+            Debug.Log("FadeInStart");
+            while((lightN.localScale.x > (lightLocalScale.x - 1)))
+            {
+                lightN.localScale -= new Vector3(0.05f, 0.05f, 0);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        else
+        {
+            Debug.Log("FadeOutStart");
+            while ((lightN.localScale.x < (lightLocalScale.x + 1)))
+            {
+                lightN.localScale += new Vector3(0.05f, 0.05f, 0);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+        yield break;
+    }
+}
